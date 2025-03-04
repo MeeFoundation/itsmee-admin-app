c3bd03bb8ad6c096dc1cf7825380fe11550f4ea6
Raman Siliuk <rsiluk@swiftinvention.com>
2025-03-04 10:55:31 +0400

    mee-components updated, added sign-up-soon page

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,6 +1,19 @@
 import { Cookie } from "mee-components";
 
-export function checkAdmin(authPages?: boolean) {
+export function checkAdmin(authPages?: boolean, signUpSoonPage?: boolean) {
+  const signUpAvailable = Cookie.getCookie("sign-up-available");
+  if (!signUpAvailable && !signUpSoonPage) {
+    window.location.hash = "sign-up-soon";
+    return false;
+  } else if (signUpAvailable && signUpSoonPage) {
+    window.location.hash = "sign-in";
+    return false;
+  }
+
+  if (signUpSoonPage) {
+    return false;
+  }
+
   const admin = Cookie.getCookie("admin");
   if (!authPages && !admin) {
     window.location.hash = "sign-in";
